a1999376eee75d116b2f14be6849f106994e3264
Piotr Owsiak <piotr.owsiak@o2.pl>
2016-09-02 22:58:53 +0200

    restore nuget packages during build

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 REM SET MSBUILD="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 REM SET MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-SET MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+REM SET MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\amd64\MSBuild.exe"
 SET SOLUTION_FILE=evtailNet.sln
 
 %MSBUILD% %SOLUTION_FILE% /m %*
